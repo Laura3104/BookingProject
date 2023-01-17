646dcb874f4ee96daa6cb1656694e42b3dc29053
Laura Carmona <laura.carmona@nordstrom.com>
2023-01-17 16:31:21 -0500

    Add controller

---
+++ src/main/java/com/example/spring/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.spring.controller;
+
+import com.example.spring.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping("/users/{id}")
+    public String users(@PathVariable("id") long id, Model model){
+        model.addAttribute("users", this.bookingService.getUserById(id));
+        return "user";
+    }
+}
